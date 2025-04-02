bf823a3c75c6268109940c55f0ea62574fcda8e6
Stefan Maron <stefanmaron@outlook.de>
2025-04-02 07:37:17 +0200

    Enhance PowerShell script to correctly parse EDMX capabilities and update YAML paths based on entity restrictions

--- fix-api-capabilities.ps1
+++ fix-api-capabilities.ps1
@@ -11,22 +11,39 @@ function Get-EntityCapabilities {
     param($edmxContent)
     
     $capabilities = @{}
+    Write-Host "Parsing EDMX capabilities..."
     
     # Load EDMX as XML
     $xml = [xml]$edmxContent
-    $entitySets = $xml.SelectNodes("//EntitySet")
+    
+    # Create XML namespace manager
+    $nsManager = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+    $nsManager.AddNamespace("edmx", "http://docs.oasis-open.org/odata/ns/edmx")
+    $nsManager.AddNamespace("edm", "http://docs.oasis-open.org/odata/ns/edm")
+    
+    # Find all EntitySet elements using namespace manager
+    $entitySets = $xml.SelectNodes("//edm:EntitySet", $nsManager)
     
     foreach ($entitySet in $entitySets) {
         $entityName = $entitySet.Name
-        $deletable = $entitySet.SelectSingleNode(".//PropertyValue[@Property='Deletable']")?.Bool -eq 'true'
-        $insertable = $entitySet.SelectSingleNode(".//PropertyValue[@Property='Insertable']")?.Bool -eq 'true'
-        $updatable = $entitySet.SelectSingleNode(".//PropertyValue[@Property='Updatable']")?.Bool -eq 'true'
+        
+        # Find capability annotations with correct paths
+        $deletable = $entitySet.SelectSingleNode(".//edm:Annotation[@Term='Org.OData.Capabilities.V1.DeleteRestrictions']//edm:PropertyValue[@Property='Deletable']", $nsManager)?.Bool -eq 'true'
+        $insertable = $entitySet.SelectSingleNode(".//edm:Annotation[@Term='Org.OData.Capabilities.V1.InsertRestrictions']//edm:PropertyValue[@Property='Insertable']", $nsManager)?.Bool -eq 'true'
+        $updatable = $entitySet.SelectSingleNode(".//edm:Annotation[@Term='Org.OData.Capabilities.V1.UpdateRestrictions']//edm:PropertyValue[@Property='Updatable']", $nsManager)?.Bool -eq 'true'
         
         $capabilities[$entityName] = @{
             Deletable = $deletable
             Insertable = $insertable
             Updatable = $updatable
         }
+        Write-Host "Found entity '$entityName': Deletable=$deletable, Insertable=$insertable, Updatable=$updatable"
+    }
+    
+    if ($capabilities.Count -eq 0) {
+        Write-Warning "No entity capabilities found in EDMX file!"
+    } else {
+        Write-Host "Found capabilities for $($capabilities.Count) entities"
     }
     
     return $capabilities
@@ -36,34 +53,49 @@ function Get-EntityCapabilities {
 function Update-YamlPaths {
     param($yaml, $capabilities)
     
-    foreach ($entityName in $capabilities.Keys) {
-        $entityCaps = $capabilities[$entityName]
-        
-        # Collection endpoint paths
-        $collectionPath = "/$entityName"
-        if ($yaml.paths.$collectionPath -and -not $entityCaps.Insertable) {
-            if ($yaml.paths.$collectionPath.post) {
-                $yaml.paths.$collectionPath.PSObject.Properties.Remove('post')
+    $modified = $false
+    Write-Host "Updating YAML paths..."
+    
+    foreach ($path in $yaml.paths.PSObject.Properties.Name) {
+        # Determine which entity this path belongs to
+        $entityName = $null
+        foreach ($entity in $capabilities.Keys) {
+            # Check if path starts with entity name (including nested paths)
+            if ($path -match "^/$entity(?:/|\(|$)") {
+                $entityName = $entity
+                break
             }
         }
-        
-        # Individual item endpoint paths
-        $itemPaths = $yaml.paths.PSObject.Properties.Name | Where-Object { 
-            $_ -like "/$entityName(*)" -and -not $_ -like "*/*" 
-        }
-        
-        foreach ($path in $itemPaths) {
-            if (-not $entityCaps.Updatable) {
-                if ($yaml.paths.$path.patch) {
-                    $yaml.paths.$path.PSObject.Properties.Remove('patch')
+
+        if ($entityName -and $capabilities.ContainsKey($entityName)) {
+            $entityCaps = $capabilities[$entityName]
+            $pathObj = $yaml.paths.$path
+            
+            # Remove POST if not insertable (for both root and nested paths)
+            if (-not $entityCaps.Insertable -and $pathObj.PSObject.Properties['post']) {
+                $pathObj.PSObject.Properties.Remove('post')
+                Write-Host "Removed POST operation from $path"
+                $modified = $true
+            }
+            
+            # Remove PATCH/DELETE for any path with parameters (both root and nested)
+            if ($path -match '\(.*\)') {  # Fixed regex pattern for matching paths with parameters
+                if (-not $entityCaps.Updatable -and $pathObj.PSObject.Properties['patch']) {
+                    $pathObj.PSObject.Properties.Remove('patch')
+                    Write-Host "Removed PATCH operation from $path"
+                    $modified = $true
+                }
+                if (-not $entityCaps.Deletable -and $pathObj.PSObject.Properties['delete']) {
+                    $pathObj.PSObject.Properties.Remove('delete')
+                    Write-Host "Removed DELETE operation from $path"
+                    $modified = $true
                 }
             }
-            if (-not $entityCaps.Deletable) {
-                if ($yaml.paths.$path.delete) {
-                    $yaml.paths.$path.PSObject.Properties.Remove('delete')
-                }
-            }
         }
+    }
+    
+    if (-not $modified) {
+        Write-Host "No changes were necessary in the YAML file"
     }
     
     return $yaml
@@ -71,11 +103,14 @@ function Update-YamlPaths {
 
 try {
     # Read input files
+    Write-Host "Reading EDMX from: $EdmxPath"
     $edmxContent = Get-Content $EdmxPath -Raw
+    
+    Write-Host "Reading YAML from: $YamlPath"
     $yamlContent = Get-Content $YamlPath -Raw
     
     # Parse YAML
-    $yaml = ConvertFrom-Json $yamlContent -AsHashtable
+    $yaml = ConvertFrom-Json $yamlContent
     
     # Get entity capabilities from EDMX
     $capabilities = Get-EntityCapabilities $edmxContent
@@ -83,10 +118,9 @@ try {
     # Update YAML paths
     $updatedYaml = Update-YamlPaths $yaml $capabilities
     
-    # Save updated YAML
+    # Save changes back to original file
     $updatedYaml | ConvertTo-Json -Depth 100 | Set-Content $YamlPath
-    
-    Write-Host "Successfully updated API capabilities in: $YamlPath"
+    Write-Host "Successfully updated $YamlPath"
 }
 catch {
     Write-Error "Error processing files: $_"
